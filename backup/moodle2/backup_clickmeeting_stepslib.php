ae3d3b65ba5b24b347997c6ab71572f9659110b1
Jakub Kowalik <jkowalik@clickmeeting.com>
2024-10-29 12:53:27 +0100

    style: HACK-1234: fix coding style

--- backup/moodle2/backup_clickmeeting_stepslib.php
+++ backup/moodle2/backup_clickmeeting_stepslib.php
@@ -14,6 +14,11 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace mod_clickmeeting;
+
+use backup;
+use backup_nested_element;
+
 /**
  * Define the complete choice structure for backup, with file and id annotations
  *
@@ -21,12 +26,6 @@
  * @copyright 2024 Clickmeeting
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-namespace mod_clickmeeting;
-
-use backup;
-use backup_nested_element;
-
 class backup_activity_structure_step extends \backup_activity_structure_step {
 
     /**
