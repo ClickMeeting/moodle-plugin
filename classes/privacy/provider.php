9f08ac75ccf6d417fa48a2910c8f2cd3d7b80c59
Jakub Kowalik <jkowalik@clickmeeting.com>
2025-01-21 13:46:38 +0100

    wip: HACK-1467: privacy api implementation - show stored data in privacy tools

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -23,18 +23,63 @@
  */
 
 namespace mod_clickmeeting\privacy;
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\approved_userlist;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\userlist;
 
 /**
- * Null provider because we do not store any personal data
+ * Ad hoc task that performs the actions for approved data privacy requests.
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\plugin\provider,
+    \core_privacy\local\request\core_userlist_provider {
+
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Returns metadata about this system.
      *
-     * @return string
+     * @param   collection $collection The collection to add metadata to.
+     * @return  collection  The array of metadata
      */
-    public static function get_reason(): string {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_database_table('clickmeeting', [
+            'name' => 'privacy:metadata:clickmeeting:name',
+            'description' => 'privacy:metadata:clickmeeting:description',
+            'lobby_msg' => 'privacy:metadata:clickmeeting:lobbymsg',
+        ], 'privacy:metadata:clickmeeting');
+
+
+        $collection->add_external_location_link('clickmeeting.com', [
+            'email' => 'privacy:metadata:clickmeeting_api:email',
+            'nickname' => 'privacy:metadata:clickmeeting_api:nickname',
+        ], 'privacy:metadata:clickmeeting_api');
+
+        return $collection;
+    }
+
+    public static function get_contexts_for_userid(int $userid): contextlist
+    {
+    }
+
+    public static function export_user_data(approved_contextlist $contextlist)
+    {
+    }
+
+    public static function delete_data_for_all_users_in_context(\context $context)
+    {
+    }
+
+    public static function delete_data_for_user(approved_contextlist $contextlist)
+    {
+    }
+
+    public static function get_users_in_context(userlist $userlist)
+    {
+    }
+
+    public static function delete_data_for_users(approved_userlist $userlist)
+    {
     }
 }
